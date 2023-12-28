26c1cbfa99abde4ae8ea683c9c6d610068fe0281
jimmyann2023 <2492617236@qq.com>
2023-12-28 14:32:00 +0800

    fix:  vite plugins

--- configs/vite/src/plugins/index.ts
+++ configs/vite/src/plugins/index.ts
@@ -1,19 +1,26 @@
-import { PluginOption } from "vite";
-import { ViteEnv } from "../utils";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import legacy from "@vitejs/plugin-legacy";
-import { configHtmlPlugin } from "./html";
-import { configMockPlugin } from "./mock";
-import { configCompressPlugin } from "./compress";
-import { configUnocssPlugin } from "./unocss";
-import { configHttpsPlugin } from "./https";
-import { createConfigPlugin } from "./config";
+import { PluginOption } from 'vite'
+import { ViteEnv } from '../utils'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import legacy from '@vitejs/plugin-legacy'
+import purgeIcons from 'vite-plugin-purge-icons'
+import { configHtmlPlugin } from './html'
+import { configMockPlugin } from './mock'
+import { configCompressPlugin } from './compress'
+import { configUnocssPlugin } from './unocss'
+import { configHttpsPlugin } from './https'
+import { createConfigPlugin } from './config'
+import { configSvgIconsPlugin } from './svg-icons'
+import { configVisualizerConfig } from './visualizer'
+import monacoEditorPlugin from 'vite-plugin-monaco-editor'
+import MonoRepoAliasPlugin from './monorepo'
+import { configImageminPlugin } from './imagesmin'
+
 
 export async function configVitePlugins(
   root: string,
   viteEnv: ViteEnv,
-  isBuild: boolean
+  isBuild: boolean,
 ) {
   const {
     VITE_USE_IMAGEMIN,
@@ -21,20 +28,20 @@ export async function configVitePlugins(
     VITE_LEGACY,
     VITE_BUILD_COMPRESS,
     VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE,
-  } = viteEnv;
+  } = viteEnv
 
   const vitePlugins: (PluginOption | PluginOption[])[] = [
     // handle .vue files
     vue(),
     // have to
     vueJsx(),
-  ];
-  console.log("isBuild", isBuild);
+  ]
+  console.log('isBuild', isBuild)
   // @vitejs/plugin-legacy
-  VITE_LEGACY && isBuild && vitePlugins.push(legacy());
+  VITE_LEGACY && isBuild && vitePlugins.push(legacy())
 
   // vite-plugin-html
-  vitePlugins.push(await configHtmlPlugin(root, viteEnv, isBuild));
+  vitePlugins.push(await configHtmlPlugin(root, viteEnv, isBuild))
 
   // unocss
   vitePlugins.push(configUnocssPlugin())
@@ -42,37 +49,37 @@ export async function configVitePlugins(
   vitePlugins.push(createConfigPlugin())
 
   // vite-plugin-svg-icons
-  // vitePlugins.push(configSvgIconsPlugin(isBuild))
+  vitePlugins.push(configSvgIconsPlugin(isBuild))
 
   // vite-plugin-mock
-  VITE_USE_MOCK && vitePlugins.push(configMockPlugin(isBuild));
+  VITE_USE_MOCK && vitePlugins.push(configMockPlugin(isBuild))
 
   // vite-plugin-purge-icons
-  // vitePlugins.push(purgeIcons());
+  vitePlugins.push(purgeIcons())
 
   // rollup-plugin-visualizer
-  // vitePlugins.push(configVisualizerConfig())
+  vitePlugins.push(configVisualizerConfig())
 
   // http2
   vitePlugins.push(configHttpsPlugin(viteEnv))
   // monacoEditorPlugin
-  // vitePlugins.push(monacoEditorPlugin({}))
+  vitePlugins.push(monacoEditorPlugin({}))
   // MonorepoSupport
-  // vitePlugins.push(MonoRepoAliasPlugin())
+  vitePlugins.push(MonoRepoAliasPlugin())
 
   // The following plugins only work in the production environment
   if (isBuild) {
     // vite-plugin-imagemin
-    // VITE_USE_IMAGEMIN && vitePlugins.push(configImageminPlugin());
+    VITE_USE_IMAGEMIN && vitePlugins.push(configImageminPlugin());
 
     // rollup-plugin-gzip
     vitePlugins.push(
       configCompressPlugin(
         VITE_BUILD_COMPRESS,
-        VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE
-      )
-    );
+        VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE,
+      ),
+    )
   }
 
-  return vitePlugins;
+  return vitePlugins
 }
